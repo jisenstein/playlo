59f29ac67f58cbffa253ab91aafa6da319278e3a
jisenstein <jisenstein@gmail.com>
2015-07-29 09:21:49 -0700

    remove force show of form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -40,6 +40,6 @@ class SessionsController < ApplicationController
       flash[:notice] = "Hit rate limit."
       raise
     end
-    Rails.cache.write(session['access_token'], verified_friend_names, expires_in: 10.minutes)
+    Rails.cache.write(session['access_token'], verified_friend_names, expires_in: 15.minutes)
   end
 end
