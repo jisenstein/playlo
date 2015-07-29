d4205fcb3b5066bad46e8c66172871ac33fc7ce1
jisenstein <jisenstein@gmail.com>
2015-07-29 03:19:13 -0700

    add style and randomness

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -2,18 +2,23 @@ class PlaylistController < ApplicationController
   def create
     spotify_tracks = []
     puts "ready to create a playlist"
+    debugger
+    type = params[:random] || "top"
+    playlist_name = params[:name] || "a playlist"
 
     friends = Rails.cache.fetch(session['access_token'])
     if !friends.blank?
       friends.each do |friend|
         artists = RSpotify::Artist.search(friend, limit: 1)
         if (a = artists[0]) && a.popularity > 20
-          top_tracks = a.top_tracks(:US)
-          if (t = top_tracks[0]) && t.popularity > 20
-            puts "Twitter name: #{friend} --- Artist name: #{a.name}"
-            puts "Artist popularity: #{a.popularity} --- Track popularity: #{t.popularity}"
-            spotify_tracks << t
-          end
+          spotify_tracks << case type
+                            when "top"
+                              a.top_tracks(:US).first
+                            when "random"
+                              a.top_tracks(:US).sample
+                            when "super_random"
+                              a.albums(limit: 7, country: 'US').sample.tracks.sample
+                            end
         end
       end
     end
@@ -21,17 +26,20 @@ class PlaylistController < ApplicationController
     puts "finished searching for tracks"
     puts "about to log into spotify"
 
-    spotify_user = RSpotify::User.new(session['spotify_credentials'])
+    if !spotify_tracks.blank?
+      spotify_user = RSpotify::User.new(session['spotify_credentials'])
 
-    puts "logged into spotify"
+      puts "logged into spotify"
 
-    playlist = spotify_user.create_playlist!('my-first-playlo-playlist')
-    playlist.change_details!(public: false)
-    playlist.add_tracks!(spotify_tracks)
+      playlist = spotify_user.create_playlist!(playlist_name)
+      playlist.change_details!(public: false)
+      playlist.add_tracks!(spotify_tracks)
+      puts "successfully created playlist"
+      flash[:notice] = "Just made #{playlist_name}!"
+    else
+      flash[:notice] = "found 0 tracks, did not create playlist"
+    end
 
-    puts "successfully created playlist"
-
-    session[:playlist_created] = true
     redirect_to root_path
   end
 end
