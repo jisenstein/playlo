b636326d1f882c5a135fb5ac57f901b3b5a6a854
jisenstein <jisenstein@gmail.com>
2015-08-02 00:19:25 -0700

    optimize

--- app/models/playlist_creator.rb
+++ app/models/playlist_creator.rb
@@ -1,26 +1,24 @@
 class PlaylistCreator
-  Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'dj.log'))
   def create_playlist(param_type, param_name, friends, spotify_credentials)
-    spotify_tracks = []
-    Delayed::Worker.logger.debug("ready to create a playlist")
     puts "ready to create a playlist"
     type = param_type || "top"
 
     playlist_name = param_name || "a playlist"
 
-    # friends = Rails.cache.fetch(cache_key)
+    spotify_user = RSpotify::User.new(spotify_credentials)
+    playlist = spotify_user.create_playlist!(playlist_name)
+    playlist.change_details!(public: false)
+
     begin
       if !friends.blank?
-        friends.each do |friend|
-          Delayed::Worker.logger.debug("fetching artist for #{friend}")
+        friends.shuffle.each do |friend|
           puts "fetching artist for #{friend}"
           artists = RSpotify::Artist.search(friend, limit: 1)
           if (a = artists[0]) && a.popularity > 20
-            Delayed::Worker.logger.debug("got artist match #{a.name}")
             puts "got artist match #{a.name}"
             track = send(type, a)
             if track.class == RSpotify::Track
-              spotify_tracks << send(type, a)
+              playlist.add_tracks!([send(type, a)])
             end
           end
         end
@@ -31,24 +29,6 @@ class PlaylistCreator
       puts "spotify rate limit"
       puts "error"
       puts error
-      puts error.retry_after
-      Delayed::Worker.logger.debug(error)
-    end
-
-    Delayed::Worker.logger.debug("about to log into spotify")
-    puts "logging into spotify"
-
-    if !spotify_tracks.blank?
-      spotify_user = RSpotify::User.new(spotify_credentials)
-
-      Delayed::Worker.logger.debug("logged into spotify")
-      puts "logged in"
-
-      playlist = spotify_user.create_playlist!(playlist_name)
-      playlist.change_details!(public: false)
-      playlist.add_tracks!(spotify_tracks.shuffle)
-      Delayed::Worker.logger.debug("successfully created playlist")
-    else
     end
   end
 
