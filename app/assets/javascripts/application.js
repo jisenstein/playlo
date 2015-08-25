98c1de0599b58f78c94191381f1b365c35b7337f
Josh Isenstein <jisenstein@gmail.com>
2015-08-24 23:58:38 -0500

    indentation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,61 +16,61 @@
 //= require_tree .
 //= require bootstrap.min
 
-  $('form.create_playlist').submit(function(e){
-    if (!$('input.form-control').val()) {
-      alert("enter a name for your playlist!");
-      return false;
+$('form.create_playlist').submit(function(e){
+  if (!$('input.form-control').val()) {
+    alert("enter a name for your playlist!");
+    return false;
+  }
+
+  $('.progress-bar').css('width', '2%');
+  $('.progress').show();
+
+  var $form = $(this);
+  var count = 0;
+  var playlist_id = -1;
+
+  $.ajax({
+    url: '/playlists?' + $form.serialize(),
+    method: 'POST',
+    complete: function(response) {
+      var json = response.responseJSON;
+      if (json.status == 200) {
+        getProgress(json.playlist_id);
+      } else {
+        location.reload();
+        showAlert(json.alert);
+      }
     }
+  });
 
-    $('.progress-bar').css('width', '2%');
-    $('.progress').show();
+  return false;
+});
 
-    var $form = $(this);
-    var count = 0;
-    var playlist_id = -1;
-
+var $progressBar = $('.progress-bar');
+function getProgress(playlist_id) {
+  var timerId = setInterval(function() {
     $.ajax({
-      url: '/playlists?' + $form.serialize(),
-      method: 'POST',
+      url: '/playlists/' + playlist_id,
+      method: 'PUT',
       complete: function(response) {
+        console.log("got response");
         var json = response.responseJSON;
-        if (json.status == 200) {
-          getProgress(json.playlist_id);
-        } else {
-          location.reload();
+        if (json.status == 400) {
+          console.log("increasing width to " + json.percent);
+          $progressBar.css('width', json.percent)
+        } else if (json.status == 200) {
+          console.log("at 100%");
+          $progressBar.css('width', '100%');
+          clearInterval(timerId);
+          $('.progress').hide();
           showAlert(json.alert);
         }
       }
     });
+  }, 3000);
+};
 
-    return false;
-  });
-
-  var $progressBar = $('.progress-bar');
-  function getProgress(playlist_id) {
-    var timerId = setInterval(function() {
-      $.ajax({
-        url: '/playlists/' + playlist_id,
-        method: 'PUT',
-        complete: function(response) {
-          console.log("got response");
-          var json = response.responseJSON;
-          if (json.status == 400) {
-            console.log("increasing width to " + json.percent);
-            $progressBar.css('width', json.percent)
-          } else if (json.status == 200) {
-            console.log("at 100%");
-            $progressBar.css('width', '100%');
-            clearInterval(timerId);
-            $('.progress').hide();
-            showAlert(json.alert);
-          }
-        }
-      });
-    }, 3000);
-  };
-
-  function showAlert(text) {
-    $('.alerts_text').empty().append(text);
-    $('.alert').show();
-  }
+function showAlert(text) {
+  $('.alerts_text').empty().append(text);
+  $('.alert').show();
+}
